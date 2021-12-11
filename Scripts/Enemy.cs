5cf4471b9ec76cfb963e5704a72bb81a189fceba
DESKTOP-K3TC50U\qbsudet <quoobt@gmail.com>
2021-12-12 03:05:52 +0900

    기본 컨트롤, Group 이용한 리스너, RigidBody collision

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class Enemy : RigidBody
+{
+    private Vector3 pos;
+    private Random random;
+    private float speed = 50;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        pos = GlobalTransform.origin;
+        pos.x += - delta * speed;
+        this.Translation = pos;
+
+        if(pos.x < -100) {
+            GetTree().CallGroup("MainListener", "score_increment");
+            QueueFree();
+        }
+    }
+}
---
+++ Scripts/Main.cs
@@ -0,0 +1,93 @@
+using Godot;
+using System;
+public class Main : Spatial
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	//플레이어
+	RigidBody player;
+	Random r = new Random();
+	float linearSpeedX  = 1500f;
+	float linearSpeedY = 2000f;
+	private int score;
+	private Label scoreLabel;
+
+	private PackedScene EnemyScene = (PackedScene)ResourceLoader.Load("res://Scenes/Enemy.tscn");
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		player = (RigidBody)GetNode("Player");
+		score = 0;
+		scoreLabel = (Label)GetNode("Control/ScoreLabel");
+	}
+
+	public override void _Input(InputEvent @event) {
+		if(@event is InputEventKey) {
+			var key = (InputEventKey) @event;
+			if(key.IsPressed() && key.IsActionPressed("ui_down")){
+				//GD.Print("left");
+				player.SetAxisVelocity(new Vector3(-linearSpeedX * GetProcessDeltaTime(), 0, 0));
+			}
+			if(key.IsPressed() && key.IsActionPressed("ui_up")){
+				player.SetAxisVelocity(new Vector3(linearSpeedX * GetProcessDeltaTime(), 0, 0));
+			}
+			if(key.IsPressed() && key.IsActionPressed("ui_left")){
+				player.SetAxisVelocity(new Vector3(0, 0, - linearSpeedX * GetProcessDeltaTime()));
+			}
+			if(key.IsPressed() && key.IsActionPressed("ui_right")){
+				player.SetAxisVelocity(new Vector3(0, 0, linearSpeedX * GetProcessDeltaTime()));
+			}
+			if(key.IsPressed() && key.IsActionPressed("ui_select")){
+				player.SetAxisVelocity(new Vector3(0, linearSpeedY * GetProcessDeltaTime(), 0));
+			}
+		}
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+	private void _on_SpawnEnemyTimer_timeout() //시간마다 spawn
+	{
+		Godot.RigidBody e;
+		e = (Godot.RigidBody)EnemyScene.Instance();
+		AddChild(e);
+		Godot.Vector3 pos = e.GlobalTransform.origin;
+		pos.x = 100;
+		pos.y = 2;
+
+		int posRand = r.Next(-5, 5) * 2;
+		pos.z = pos.z - posRand;
+
+		Godot.Vector3 scale = e.Scale;
+		int scaleRand = r.Next(1, 3) * 3;
+		scale = scale * scaleRand;
+		e.Scale = scale;
+
+		e.Translation = pos;
+
+	}
+
+	public void score_increment() {
+		score += 1;
+
+		scoreLabel.Text = score.ToString();
+	}
+
+	private void _on_Player_body_entered(Godot.RigidBody body) {
+		if(body.IsInGroup("enemyGroup")) {
+			GD.Print("Enemy contacted!");
+		}
+		else {
+			//add heart and etc
+			//if(hp > 0) GetTree().ReloadCurrentScene();
+		}
+	}
+}
+
+
+
