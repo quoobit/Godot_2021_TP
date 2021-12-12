837e3b9c90459fdb86269d93c7f3cbc7d126e242
JoonHo <junohb3@naver.com>
2021-12-12 12:38:26 +0900

    Add gitignore, model

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -3,26 +3,26 @@ using System;
 
 public class Enemy : RigidBody
 {
-    private Vector3 pos;
-    private Random random;
-    private float speed = 50;
+	private Vector3 pos;
+	private Random random;
+	private float speed = 50;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        pos = GlobalTransform.origin;
-        pos.x += - delta * speed;
-        this.Translation = pos;
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		pos = GlobalTransform.origin;
+		pos.x += - delta * speed;
+		this.Translation = pos;
 
-        if(pos.x < -100) {
-            GetTree().CallGroup("MainListener", "score_increment");
-            QueueFree();
-        }
-    }
+		if(pos.x < -100) {
+			GetTree().CallGroup("MainListener", "score_increment");
+			QueueFree();
+		}
+	}
 }
